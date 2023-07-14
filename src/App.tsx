5ec818759bb39087dc4b0efbaca7c932c7256cea
Felipe Duarte <feduarte.dev@gmail.com>
2023-07-14 12:17:08 -0300

    terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [randomDog, setRandomDog] = useState('');
+  const [btnClick, setBtnClick] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setRandomDog(data.message);
+      setLoading(false);
+
+      alert((data.message).split('/')[4]);
+      localStorage.setItem('dogUrl', JSON.stringify(data.message));
+    }
+
+    fetchData();
+  }, [btnClick]);
+
+  function handleName() {
+    return btnClick ? setBtnClick(false) : setBtnClick(true);
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div>
       <h1>Doguinhos</h1>
+      <img src={ randomDog } alt="Doguinho aleatório" />
+      <button onClick={ handleName }>Novo doguinho!</button>
     </div>
   );
 }
